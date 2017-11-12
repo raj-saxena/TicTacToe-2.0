3ae12b8ab4a3fe36dfec60f299f14674bafebb42
Raj <the.raj.saxena@gmail.com>
2017-11-12 18:13:42 +0100

    Display board state

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -1,9 +1,27 @@
-
-
 class Board(size: Int) {
-  type Symbol = Option[String]
-  type Row = Seq[Symbol]
+  type Symbol = String
+  type Cell = Option[Symbol]
+  type Row = Seq[Cell]
   type GameBoard = Seq[Row]
 
   val state: GameBoard = for (_ <- 0 until size) yield for (_ <- 0 until size) yield None
+
+  private def getHorizontalSymbols(row: Row) = for (cell <- row) yield cell
+
+  def getStateStr: String = {
+    val symbolRows = for {
+      row <- state
+    } yield getHorizontalSymbols(row) map {
+      case Some(p) => p
+      case _ => "_"
+    } mkString " | "
+
+    symbolRows mkString "\n"
+  }
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println(new Board(3).getStateStr)
+  }
 }
--- src/test/scala/BoardSpec.scala
+++ src/test/scala/BoardSpec.scala
@@ -4,6 +4,15 @@ class BoardSpec extends FlatSpec with Matchers {
   "Board" should "be empty when created" in {
     val state = new Board(3).state
 
-    state should be (Vector(Vector(None, None, None), Vector(None, None, None), Vector(None, None, None)))
+    state should be(Vector(Vector(None, None, None), Vector(None, None, None), Vector(None, None, None)))
+  }
+
+  it should "return board state as string" in {
+    val board = new Board(3)
+
+    board.getStateStr should be(
+      """_ | _ | _
+        |_ | _ | _
+        |_ | _ | _""".stripMargin)
   }
 }
